accfb0d3bbfa8f24d79848b4b0bcec8425dfd228
qaselimgencer <qaselimgencer@gmail.com>
2026-05-04 15:36:35 +0300

    ci: update environment variables and branch focus

--- tests/ui/ui_test_06_contact_us.spec.js
+++ tests/ui/ui_test_06_contact_us.spec.js
@@ -8,12 +8,13 @@ test.describe("Test Case 6 - Contact Us Form", () => {
     const homePage = new HomePage(page);
     const contactUsPage = new ContactUsPage(page);
 
-    // Test data
-    const baseUrl = "https://automationexercise.com/";
-    const userName = "Selim Tester";
-    const testEmail = "selim.tester@example.com";
-    const filePath =
-      "C:\\Users\\LENOVO\\Downloads\\Karakaya_1923_Sitesi_Denetim_Raporu.docx";
+    
+    const baseUrl = process.env.BASE_URL;
+    const userName = process.env.USER_NAME; // "Riza Gencalp" gelecek
+    const testEmail = process.env.TEST_EMAIL; // "rizagencalp@gmail.com" gelecek
+
+    //we need to upload a file, so we create a test file in the assets folder and get its path
+    const filePath = path.resolve(process.cwd(), "assets", "upload-test.txt");
 
     // Step 2: Navigate to URL
     await page.goto(baseUrl);
@@ -21,37 +22,32 @@ test.describe("Test Case 6 - Contact Us Form", () => {
     // Step 3: Click 'Contact Us' button
     await homePage.clickContactUs();
 
-    // Step 4: Verify 'Get In Touch' is visible
+    // Step 4: Verify 'Get In Touch' visible
     await expect(contactUsPage.heading).toBeVisible();
 
-    // Step 5: Fill form + upload file
+    // Step 5: Fill the form
     await contactUsPage.fillForm(
       userName,
       testEmail,
-      "Test Subject",
-      "This is a test message.",
+      "Technical Support Request",
+      "This is an automated test message from Riza Gencalp.",
       filePath,
     );
 
-    await expect(contactUsPage.fileInput).toHaveValue(
-      /Karakaya_1923_Sitesi_Denetim_Raporu\.docx/,
-    );
+    // File verification (upload-test.txt)
+    await expect(contactUsPage.fileInput).toHaveValue(/upload-test\.txt/);
 
-    // CRITICAL:Alert handle needed for file upload, otherwise test will fail due to unhandled alert
-      await contactUsPage.submitForm();
+    // Submit the form (already handled in the Dialog PageObject)
+    await contactUsPage.submitForm();
 
     // Step 10: Verify success message
     const successLocator = contactUsPage.successMessage;
-
     await expect(successLocator).toBeVisible({ timeout: 15000 });
     await expect(successLocator).toContainText(/success/i);
 
     // Step 11: Click Home and verify
     await contactUsPage.clickHomeButton();
-
-    // fallback click (overlay ihtimaline karşı)
     await page.mouse.click(10, 10);
-
     await expect(homePage.slider).toBeVisible();
   });
 });
